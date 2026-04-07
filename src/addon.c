3856ee4c816ab982dfe575f3f32b436830594b3d
Alex Chiomny <alexchiomny@Alexs-MacBook-Air.local>
2026-04-07 14:26:30 -0400

    adding some files

---
+++ src/addon.c
@@ -0,0 +1,17 @@
+/*
+ * addon.c
+ *
+ *  Created on: Apr 6, 2026
+ *      Author: alexchiomny
+ */
+
+
+#include "addon.h"
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int multiply(int a, int b) {
+    return a * b;
+}
